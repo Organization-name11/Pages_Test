603eeddcd7ed1118955748e22976485215469a97
HarutoGitTsukushi <19921205yuki@gmail.com>
2025-12-16 14:13:38 +0000

    ok

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xgRGcXj62tpAgBonQG0YDhzTQ+4lj6CMCcPyc/hcoSU=\"\n}"
---
+++ out/_next/static/ROZnAqJjL-2BUEZZIC0Kg/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/850e41606cf4c331.js"
+  ],
+  "/_error": [
+    "static/chunks/de73d640635c34a5.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/spatial-id"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
