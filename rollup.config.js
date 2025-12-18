a521fe002b25463fca614334c1b9ec56aebc6971
HarutoGitTsukushi <19921205yuki@gmail.com>
2025-12-18 04:43:55 +0000

    ok

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+
+// rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+
+export default {
+  input: 'ouranos-gex-lib-for-JavaScript/src/index.ts',
+  output: {
+    file: 'docs/lib/index.umd.js',
+    format: 'umd',
+    name: 'SpatialId',       // ← これが window.SpatialId になります
+    sourcemap: true,
+  },
+  plugins: [
+    resolve({ browser: true }),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    terser(),
+  ],
+}
